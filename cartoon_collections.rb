115a1b575904d2476258eef2772bd802324b1d17
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-19 23:46:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,10 @@ def summon_captain_planet(words)
   captains_words
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.all? do |call|
+    call.length <= 4
+  end
 end
 
 def find_the_cheese# code an argument here
