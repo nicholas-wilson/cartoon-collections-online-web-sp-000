2d9bcf7c303c6d45110942e8164d8c1d654ce547
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-20 00:27:03 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,34 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_index do |dwarf|
+    puts "#{dwarf + 1}. #{dwarves[dwarf]}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(words)
+  captains_words = []
+  words.each do |word|
+    captains_words << "#{word.capitalize}!"
+  end
+  captains_words
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? do |call|
+    call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheeses)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheeses.each do |cheese|
+    type_index = 0
+    while type_index < cheese_types.length
+      if (cheese == cheese_types[type_index])
+        return cheese
+      end
+      type_index += 1
+    end
+  end
+  nil
 end
